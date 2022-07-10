6a0d15a42edd1bf78b2cb329d62441f815dadbff
Максим Васильев <94627655+puskainrestpease@users.noreply.github.com>
2022-07-10 18:49:17 +0300

    FIX #1

--- calc.py
+++ calc.py
@@ -6,13 +6,13 @@ try:
     from termcolor import cprint
     cprint(r'All modules ok!', 'green')
     time.sleep(0.5)
-    os.system('clear')
 except ModuleNotFoundError or ImportError as er:
     exit(f'{er}\ncheck out imports!')
 
 
 if __name__ == '__main__':
     try:
+        os.system('clear')
         print(eval(input(f'''Input here you'r example: ''')))
     except Exception as er:
         if 'unexpected EOF while parsing (<string>, line 0)' or 'unexpected EOF while parsing (<string>, line 1)' in er:
